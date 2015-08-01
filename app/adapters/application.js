3f1e59fb0b1c8974ec9ca5970af9b4144cccf082
santiago <santiago@codecantor.com>
2015-08-01 17:56:04 +0200

    Add an adapter

---
+++ app/adapters/application.js
@@ -0,0 +1,8 @@
+//
+// Import the default export from active-model-adapter
+//
+import ActiveModelAdapter from 'active-model-adapter';
+
+export default ActiveModelAdapter.extend({
+  namespace: 'api'
+});
