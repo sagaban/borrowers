f9babf08a838947cd55919299248739489786726
santiago <santiago@codecantor.com>
2015-08-04 10:27:16 +0200

    Add delete action

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -10,5 +10,12 @@ export default Ember.Route.extend({
   //
   model() {
     return this.store.findAll('friend');
+  },
+
+  actions: {
+    delete(friend) {
+      friend.destroyRecord();
+      return false;
+    }
   }
 });
