63a8a6da3c81744195f9823033c40af98e09cc50
santiago <santiago@codecantor.com>
2015-08-08 12:57:23 +0200

    Remove ic-ajax library and go back to ember-cli-ic-ajax addon. Replace moment to a custom library

--- app/utils/date-helpers.js
+++ app/utils/date-helpers.js
@@ -1,6 +1,9 @@
+import { format as borrowersDate } from 'borrowers-dates';
+
 function formatDate(date, format) {
-  return window.moment(date).format(format);
+  return borrowersDate(date, format);
 }
+
 export {
   formatDate
 };
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -37,7 +37,7 @@ module.exports = function (defaults) {
 
   app.import('bower_components/moment/moment.js');
 
-  app.import('bower_components/ic-ajax/dist/named-amd/main.js');
+  app.import('bower_components/borrowers-dates/index.js');
 
   return app.toTree();
 }
