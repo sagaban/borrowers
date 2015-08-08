fc3a9e149995091de5c598048a268dcf3dd96be6
santiago <santiago@codecantor.com>
2015-08-08 11:08:08 +0200

    Implement actions Ember.Route hooks, that do not do anythig, for now

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -2,17 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.createRecord('friend');
-  },
-
-  actions: {
-    save() {
-      console.log('+-- save action bubbled up to friends route');
-      return true;
+      return this.store.createRecord('friend');
     },
-    cancel() {
-      console.log('+-- cancel action bubbled up to friends route');
-      return true;
-    }
-  },
+    activate() {
+      console.log('----- activate hook called -----');
+    },
+    deactivate() {
+      console.log('----- deactivate hook called -----');
+    },
+    resetController: function(controller, isExiting, transition) {
+      if (isExiting) {
+        console.log('----- resetController hook called -----');
+      }
+    },
+
+    actions: {
+      save() {
+          console.log('+-- save action bubbled up to friends route');
+          return true;
+        },
+        cancel() {
+          console.log('+-- cancel action bubbled up to friends route');
+          return true;
+        }
+    },
 });
