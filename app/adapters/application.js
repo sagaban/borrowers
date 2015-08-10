140e7c557e017c2d4b50c5d8ed4078731ed683d9
santiago <santiago@codecantor.com>
2015-08-10 20:11:51 +0200

    Implement models relatioship loaded by link

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,6 +4,6 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-  namespace: 'api/v3',
+  namespace: 'api/v4',
   coalesceFindRequests: true
 });
